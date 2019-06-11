b25048148c27a8f8fb7ccb27390d27e4a64b6ead
Sophie McCall <sophie@fabulas.io>
2019-06-10 20:54:19 -0500

    created kaiju-list component

---
+++ src/app/kaiju-list/kaiju-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kaiju-list',
+  templateUrl: './kaiju-list.component.html',
+  styleUrls: ['./kaiju-list.component.css']
+})
+export class KaijuListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
