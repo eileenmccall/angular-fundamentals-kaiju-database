4356e6d8156aa5b57fd7d7a882ca17ded09232db
Sophie McCall <sophie@fabulas.io>
2019-06-12 12:42:36 -0500

    fixed quotes

--- src/app/kaiju-list/kaiju-list.component.ts
+++ src/app/kaiju-list/kaiju-list.component.ts
@@ -1,22 +1,22 @@
-import { Component, OnInit, OnDestroy, OnChanges } from "@angular/core";
+import { Component, OnInit, OnDestroy, OnChanges } from '@angular/core';
 
 @Component({
-  selector: "app-kaiju-list",
-  templateUrl: "./kaiju-list.component.html",
-  styleUrls: ["./kaiju-list.component.css"]
+  selector: 'app-kaiju-list',
+  templateUrl: './kaiju-list.component.html',
+  styleUrls: ['./kaiju-list.component.css']
 })
 export class KaijuListComponent implements OnInit, OnChanges, OnDestroy {
   constructor() {}
 
   ngOnInit() {
-    console.log("onInit fired!");
+    console.log('onInit fired!');
   }
 
   ngOnChanges() {
-    console.log("onChanges fired!");
+    console.log('onChanges fired!');
   }
 
   ngOnDestroy() {
-    console.log("onDestroy fired!");
+    console.log('onDestroy fired!');
   }
 }
