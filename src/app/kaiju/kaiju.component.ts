e202edfa10e96a3b7d4c457e44680731dc388add
Sophie McCall <sophie@fabulas.io>
2019-06-12 13:40:46 -0500

    added delete eventemitter, bound button to it

--- src/app/kaiju/kaiju.component.ts
+++ src/app/kaiju/kaiju.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-kaiju',
@@ -9,6 +9,7 @@ export class KaijuComponent implements OnInit {
   constructor() {}
 
   @Input() kaiju: any;
+  @Output() delete = new EventEmitter();
 
   ngOnInit() {}
 }
