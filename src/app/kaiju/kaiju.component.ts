d22f7405bc469c824fcdcbe2e915df4f5cfce887
Sophie McCall <sophie@fabulas.io>
2019-06-12 13:25:30 -0500

    binding kaiju list item to kaiju component

--- src/app/kaiju/kaiju.component.ts
+++ src/app/kaiju/kaiju.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-kaiju',
@@ -6,10 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./kaiju.component.css']
 })
 export class KaijuComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  @Input() kaiju: any;
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
